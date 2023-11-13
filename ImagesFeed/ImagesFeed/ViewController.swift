6c0b144e37a6b5d420cce974276c51be412031fa
LolaChernysheva <lolita-chernysheva@mail.ru>
2023-11-13 15:46:38 +0700

    Initial commit

---
+++ ImagesFeed/ImagesFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImagesFeed
+//
+//  Created by Lolita Chernysheva on 13.11.2023.
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
